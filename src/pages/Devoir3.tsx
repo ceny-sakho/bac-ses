9866440097d939642e18a2d2e2db09a5e772ac8a
ceny-sakho <ceny.sakho@gmail.com>
2025-11-15 18:07:57 +0100

    Fix: Use GitHub raw URLs for Devoir3 PDF

--- src/pages/Devoir3.tsx
+++ src/pages/Devoir3.tsx
@@ -5,7 +5,7 @@ const Devoir3 = () => {
         <h1 className="text-4xl font-bold text-gray-800 mb-6">Devoir 3 - Terminale</h1>
         <div className="bg-white rounded-lg shadow-lg p-4">
           <object
-            data="/devoirs/terminale/chapitre1/devoir3.pdf"
+            data="https://raw.githubusercontent.com/ceny-sakho/bac-ses/main/public/devoirs/terminale/chapitre1/devoir3.pdf"
             type="application/pdf"
             width="100%"
             height="800px"
@@ -14,7 +14,7 @@ const Devoir3 = () => {
             <p className="text-center py-8">
               Votre navigateur ne supporte pas l'affichage des PDF.{' '}
               <a
-                href="/devoirs/terminale/chapitre1/devoir3.pdf"
+                href="https://raw.githubusercontent.com/ceny-sakho/bac-ses/main/public/devoirs/terminale/chapitre1/devoir3.pdf"
                 className="text-blue-600 hover:underline"
                 download
               >
