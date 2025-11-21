44f7cb24df9a0067a925fbe5676b5f89bfd79323
ceny-sakho <ceny.sakho@gmail.com>
2025-11-21 23:32:16 +0100

    Rendre les onglets Devoir1/2/3 cliquables pour tous les chapitres
    Ajout de onClick avec navigate() pour rediriger vers /devoir1, /devoir2, /devoir3 quand on clique sur les onglets. Cela fonctionne pour tous les chapitres de Terminale, Première et Seconde.

--- src/components/devoirs/DevoirsViewer.tsx
+++ src/components/devoirs/DevoirsViewer.tsx
@@ -39,10 +39,9 @@ export const DevoirsViewer: React.FC<DevoirsViewerProps> = ({
             
             <Tabs defaultValue="devoir1" className="w-full">
               <TabsList className="w-full mb-6">
-                <TabsTrigger value="devoir1" className="flex-1">Devoir 1</TabsTrigger>
-                <TabsTrigger value="devoir2" className="flex-1">Devoir 2</TabsTrigger>
-                <TabsTrigger value="devoir3" className="flex-1">Devoir 3</TabsTrigger>
-              </TabsList>
+                <TabsTrigger value="devoir1" className="flex-1" onClick={() => navigate('/devoir1')}>Devoir 1</TabsTrigger>                <TabsTrigger value="devoir2" className="flex-1">Devoir 2</TabsTrigger>
+                <TabsTrigger value="devoir2" className="flex-1" onClick={() => navigate('/devoir2')}>Devoir 2</TabsTrigger>
+                              <TabsTrigger value="devoir3" className="flex-1" onClick={() => navigate('/devoir3')}>Devoir 3</TabsTrigger></TabsList>
 
               <TabsContent value="devoir1" className="mt-0">
                 {devoirs[0] ? (
